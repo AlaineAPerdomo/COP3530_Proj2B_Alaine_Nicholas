96db733b28374d9874495ed28a059215ca944400
epicbenfrog <nickchaves417@gmail.com>
2026-03-28 16:07:21 -0400

    class for each song. i can change things around

---
+++ Song.h
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class Song{
+    public:
+    string name;
+    string artist;
+    int year;
+    float danceability;
+    float energy;
+    float loudness;
+};
